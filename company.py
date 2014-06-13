ab557962411094328eb9cc5537c4389c6fcb5ff3
Thomas Levine <_@thomaslevine.com>
2014-06-13 10:30:41 -0400

    hashing

--- company.py
+++ company.py
@@ -4,11 +4,15 @@ import requests
 from picklecache import cache
 
 @cache(os.path.join(os.path.expanduser('~'), '.company'))
-def get(country_code, query):
+def get(country_code, key, unhashed = None):
     '''
     country_code :: Open Corporates country code | None
-    query :: str
+    key :: str
+    unhashed :: Maybe str
+
+    If the company has a weird name, pass another thing as the key and the actual value as the unhashed.
     '''
+    query = key if unhashed == None else query
     baseurl = 'https://opencorporates.com/reconcile'
     url = baseurl if country_code == None else base_url + '/' + country_code
     params = {'query': query}
